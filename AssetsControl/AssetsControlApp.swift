a5c85eaeeb588eea18a6a69eda8e7a99a9dc3843
wingsmight <wingsmight@gmail.com>
2023-03-28 04:18:30 +0700

    init project

---
+++ AssetsControl/AssetsControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetsControlApp.swift
+//  AssetsControl
+//
+//  Created by Igoryok on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AssetsControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
