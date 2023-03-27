acc5cd0686325d57009482742842df8452f3254b
wingsmight <wingsmight@gmail.com>
2023-03-28 05:10:14 +0700

    create project structure

--- AssetsControl/Views/MainView.swift
+++ AssetsControl/Views/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  AssetsControl
 //
 //  Created by Igoryok on 28.03.2023.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
