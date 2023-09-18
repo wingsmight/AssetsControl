6165870876364a7a0e726f6684e0a423cffa76bb
wingsmight <wingsmight@gmail.com>
2023-09-18 13:17:55 +0600

    fix bug: created expnse's curreny isn't updated when change source

--- AssetsControl/Views/ExpensesTab/ExpenseCreationView.swift
+++ AssetsControl/Views/ExpensesTab/ExpenseCreationView.swift
@@ -77,6 +77,9 @@ struct ExpenseCreationView: View {
 
             moneyCurrency = firstMoneyHolder.initialMoney.currency
         }
+        .onChange(of: moneyHolderSource) { newSelectedMoneyHolderSource in
+            moneyCurrency = newSelectedMoneyHolderSource.initialMoney.currency
+        }
     }
 
     private var nameField: some View {
